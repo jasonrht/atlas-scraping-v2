ed9d368ae7e69ff1a418792e9ec5c79e626e8527
jasonrht <Jason@Jasons-MacBook-Pro.local>
2022-08-04 09:13:43 +0200

    fixed email bug

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     apd.main()
     utr.main()
 
-    sm.send_m('jtsangsolutions@gmail.com')
+    sm.send_m('jtsangsolutions@gmail.com',[''])
 
     t1 = dt.datetime.now()
     print(f'Script run in {t1-t0}')
